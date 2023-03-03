268b5bb5cec222a5adce19a0beee0c49a489082b
DELL <DELL@DESKTOP-MGJDHRS>
2023-03-03 10:43:46 +0530

    welcome

---
+++ cpglearner/src/cpglearner/add.java
@@ -0,0 +1,16 @@
+package cpglearner;
+
+public class add {
+
+	public static void main(String[] args)//function- java called method 
+	{
+		// TODO Auto-generated method stub
+		//Welcome JFS Developer
+		System.out.println("Welcome JFS Developer");
+		System.out.println("Adding two numbers");
+		int a=10, b=10;
+		System.out.println("Added: "+(a+b)); //+  -  addition as well as concatenation
+	// best case; worst case ;  average case	
+	}
+
+}
